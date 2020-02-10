1b785d8f82c8521f484fd31a55a3c556b7a5f7aa
unknown <dcoll657@CSNET.local>
2020-02-10 10:13:57 -0700

    cant run it

--- src/app/App.java
+++ src/app/App.java
@@ -1,7 +1,17 @@
 package app;
+import java.io.*;
+import java.net.*;
+
 
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello Java");
+        String sentence;
+        String modifiedSentence;
+
+        BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
+
+        Socket clientSocket = new Socket("10.7.42.78", 6789);
+        DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
     }
 }
